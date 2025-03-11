6e945970c3e0692698f8cd982a4772271779a31a
camilo1895 <camiloguarnizo1895@gmail.com>
2025-03-11 08:16:10 -0500

    Corrigiendo estructura de carpetas

--- src/dtos/loginUser.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { CreateUserDto } from './createUser.dto';
-
-export class LoginUserDto extends PickType(CreateUserDto, [
-  'email',
-  'password',
-]) {}
