0e3fa9e9439ff08563527043a929d4985856501c
camilo1895 <camiloguarnizo1895@gmail.com>
2025-04-02 22:29:11 -0500

    Actividad 2 completada

---
+++ back/ecommerce-camilo1895/src/middleware/logger.middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function LoggerGlobal(req: Request, res: Response, next: NextFunction) {
+  const fecha = new Date();
+  console.log(
+    `Se ejecuto un metodo ${req.method} en la ruta ${req.url} en la fecha y hora ${fecha.toLocaleString()}`,
+  );
+}
---
+++ back/ecommerce-camilo1895/src/users/users.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  getUsers() {
+    throw new Error('Method not implemented.');
+  }
+}
