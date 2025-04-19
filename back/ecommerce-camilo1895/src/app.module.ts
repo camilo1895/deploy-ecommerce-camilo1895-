41da2ec84f2c72886b52e43dfca88d0852439074
camilo1895 <camiloguarnizo1895@gmail.com>
2025-04-18 19:30:04 -0500

    Se configura coneccion a la base de dataos

--- back/ecommerce-camilo1895/src/app.module.ts
+++ back/ecommerce-camilo1895/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const dbConfig = configService.get<TypeOrmModuleOptions>('typeorm');
+        if (!dbConfig) throw new Error('Database configuration is missing');
+        return dbConfig;
+      },
+    }),
+    ProductsModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
